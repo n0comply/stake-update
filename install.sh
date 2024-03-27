9c32df7cef7e7a6d80f51b631082dc180fd0d6e8
n0comply <165176627+n0comply@users.noreply.github.com>
2024-03-27 15:56:07 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-cd /root
+cd /root/stake-update/
 mkdir logs
-cd /root/scripts &&
+cd /root/stake-update/scripts &&
 chmod +x pm2stake.exp && ./pm2stake.exp
 pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib &&
 chmod +x runsheets.sh &&
